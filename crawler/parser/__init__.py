2cc9f0657582a2b4ed9ddfb091673a214b5db86b
dy8r <ivan@dmgblockchain.com>
2025-06-01 16:01:26 -0500

    start parser

--- crawler/parser/__init__.py
+++ crawler/parser/__init__.py
@@ -1,5 +1,15 @@
-from parser import payworks, skipthedishes, neo
+import time
+from parser import payworks, skipthedishes, neo, priceline, bold, pollard
+import random
 
 def main():
     payworks.extract_job_postings()
     neo.extract_job_postings()
+    priceline.extract_job_postings()
+    bold.extract_job_postings()
+    pollard.extract_job_postings()
+
+if __name__ == "__main__":
+    while True:
+        main()
+        time.sleep(random.randint(60, 300))  # Sleep for a random time between 1 and 5 minutes
